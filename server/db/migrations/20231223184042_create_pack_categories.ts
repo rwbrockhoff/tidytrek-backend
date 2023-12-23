9b9c3221754b2c3aba79b646efbaab9a27cecb89
rwbrockhoff <brockhoffrw@gmail.com>
2023-12-23 13:00:15 -0700

    feat: new knex migrations for pack tables

---
+++ server/db/migrations/20231223184042_create_pack_categories.ts
@@ -0,0 +1,21 @@
+import type { Knex } from "knex";
+const tableName = "pack_categories";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable(tableName, (table) => {
+    table.increments("pack_category_id").unsigned().primary();
+    table.string("pack_category_name").notNullable();
+    table.text("pack_category_description").nullable();
+    table.integer("pack_category_placement_index").notNullable();
+    table.integer("user_id").unsigned().notNullable();
+    table
+      .foreign("user_id")
+      .references("user_id")
+      .inTable("users")
+      .onDelete("CASCADE");
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable(tableName);
+}
