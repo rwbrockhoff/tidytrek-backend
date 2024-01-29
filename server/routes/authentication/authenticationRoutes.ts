31d58f371753e93654dc2196d5f2d35a3e3c046d
rwbrockhoff <brockhoffrw@gmail.com>
2024-01-29 14:00:05 -0700

    fix: confirm reset password is PUT not POST

--- server/routes/authentication/authenticationRoutes.ts
+++ server/routes/authentication/authenticationRoutes.ts
@@ -9,7 +9,7 @@ router.post('/login', authenticationController.login);
 router.post('/logout', authenticationController.logout);
 router.put('/password', authenticationController.changePassword);
 router.post('/reset-password/request', authenticationController.requestResetPassword);
-router.post('/reset-password/confirm', authenticationController.confirmResetPassword);
+router.put('/reset-password/confirm', authenticationController.confirmResetPassword);
 router.delete('/account', authenticationController.deleteAccount);
 
 export default router;
