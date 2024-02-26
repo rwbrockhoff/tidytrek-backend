8490ae9f0c06c15881c470dd2a37cf4923b93cf4
rwbrockhoff <brockhoffrw@gmail.com>
2024-02-26 16:49:15 -0700

    fix: build process

--- server/config/config.ts
+++ server/config/config.ts
@@ -1,4 +1,5 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
+
 dotenv.config({
-  path: process.env.NODE_ENV === "production" ? "production.env" : "dev.env",
+	path: process.env.NODE_ENV === 'production' ? 'production.env' : 'dev.env',
 });
