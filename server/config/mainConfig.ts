3edcdcaf143f2b2c6e0f3265774e3a1f5fc3cd83
rwbrockhoff <brockhoffrw@gmail.com>
2025-07-04 09:43:50 -0600

    feat: add morgan logger to mainConfig

--- server/config/mainConfig.ts
+++ server/config/mainConfig.ts
@@ -1,28 +1,39 @@
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import { Express } from "express";
-import { acceptedOrigins, corsErrorMessage } from "./configurationVariables.js";
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import { Express } from 'express';
+import { acceptedOrigins, corsErrorMessage } from './configurationVariables.js';
 
 const mainConfig = (app: Express) => {
-  app.use(express.urlencoded({ extended: false }));
-  app.use(express.json());
-  app.use(
-    cors({
-      credentials: true,
-      origin: (origin, cb) => {
-        //testing
-        if (!origin) return cb(null, true);
+	// HTTP request logging - skip during tests
+	if (process.env.NODE_ENV !== 'test') {
+		const logFormat =
+			process.env.NODE_ENV === 'production'
+				? ':method :url :status :res[content-length] - :response-time ms' // production
+				: 'dev'; // dev (colored messages)
 
-        if (!acceptedOrigins.includes(origin)) {
-          return cb(new Error(corsErrorMessage), true);
-        }
-        //return true if accepted origin
-        return cb(null, true);
-      },
-    })
-  );
-  app.use(cookieParser(process.env.COOKIE_SECRET));
+		app.use(morgan(logFormat));
+	}
+
+	app.use(express.urlencoded({ extended: false }));
+	app.use(express.json());
+	app.use(
+		cors({
+			credentials: true,
+			origin: (origin, cb) => {
+				//testing
+				if (!origin) return cb(null, true);
+
+				if (!acceptedOrigins.includes(origin)) {
+					return cb(new Error(corsErrorMessage), true);
+				}
+				//return true if accepted origin
+				return cb(null, true);
+			},
+		}),
+	);
+	app.use(cookieParser(process.env.COOKIE_SECRET));
 };
 
 export default mainConfig;
